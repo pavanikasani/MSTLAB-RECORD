a4b00f23473e68474b475a8632e7989f02440446
pavanikasani <131016860+pavanikasani@users.noreply.github.com>
2023-06-12 16:21:05 +0530

    Delete 5c(validate).js

--- 5c(validate).js
+++
@@ -1,7 +0,0 @@
-import { User } from './login.js';
-document.getElementById('btn').addEventListener('click',() =>{
-let username = document.getElementById('name').value;
-let password = document.getElementById('password').value;
-let user1 = new User("abc",'123');
-document.writeln(user1.validateUser(username,password));
-});
